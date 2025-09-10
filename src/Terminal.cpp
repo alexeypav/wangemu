b3ce53e6d5d6c3bb338630bbf9ca64cb3797e4b5
alexey <adpavlenko@gmail.com>
2025-09-10 21:26:41 +1200

    working build + minor additions not live

--- src/Terminal.cpp
+++ src/Terminal.cpp
@@ -26,6 +26,7 @@
 
 #include "IoCardKeyboard.h"
 #include "IoCardTermMux.h"
+//#include "SerialPort.h"
 #include "Scheduler.h"
 #include "Terminal.h"
 #include "Ui.h"
@@ -46,6 +47,7 @@ Terminal::Terminal(std::shared_ptr<Scheduler> scheduler,
                    bool vp_cpu) :
     m_scheduler(scheduler),
     m_muxd(muxd),
+    //m_serialPort(nullptr),
     m_vp_cpu(vp_cpu),
     m_io_addr(io_addr),
     m_term_num(term_num)
@@ -78,13 +80,58 @@ Terminal::Terminal(std::shared_ptr<Scheduler> scheduler,
     }
 }
 
+// Constructor for COM port terminal mode
+// Terminal::Terminal(std::shared_ptr<Scheduler> scheduler,
+//                    std::shared_ptr<SerialPort> serialPort,
+//                    int io_addr, int term_num, ui_screen_t screen_type) :
+//     m_scheduler(scheduler),
+//     m_muxd(nullptr),
+//     m_serialPort(serialPort),
+//     m_vp_cpu(false),  // COM port terminals don't need CPU throttling
+//     m_io_addr(io_addr),
+//     m_term_num(term_num)
+// {
+//     m_disp.screen_type = screen_type;
+//     m_disp.chars_w  = (screen_type == UI_SCREEN_64x16)  ? 64 : 80;
+//     m_disp.chars_h  = (screen_type == UI_SCREEN_64x16)  ? 16 : 24;
+//     m_disp.chars_h2 = (screen_type == UI_SCREEN_2236DE) ? 25 : m_disp.chars_h;
+
+//     reset(true);
+
+//     m_wndhnd = UI_displayInit(screen_type, m_io_addr, m_term_num, &m_disp);
+//     assert(m_wndhnd);
+
+//     const bool smart_term = (screen_type == UI_SCREEN_2236DE);
+//     if (smart_term) {
+//         // Register keyboard callback for COM port mode
+//         // Use a unique address for COM terminals to avoid conflicts
+//         int kb_addr = 0x1000 + io_addr; // Use high address space for COM terminals
+//         system2200::registerKb(
+//             kb_addr, m_term_num,
+//             std::bind(&Terminal::receiveKeystroke, this, std::placeholders::_1)
+//         );
+
+//         // COM port terminals send init sequence immediately
+//         m_init_tmr = m_scheduler->createTimer(
+//                        TIMER_MS(100),
+//                        std::bind(&Terminal::sendInitSeq, this)
+//                      );
+//     }
+// }
 
 // free resources on destruction
 Terminal::~Terminal()
 {
     const bool smart_term = (m_disp.screen_type == UI_SCREEN_2236DE);
     if (smart_term) {
-        system2200::unregisterKb(m_io_addr+0x01, m_term_num);
+        if (m_muxd) {
+            // MUX mode
+            system2200::unregisterKb(m_io_addr+0x01, m_term_num);
+        } else if (m_serialPort) {
+            // COM port mode
+            int kb_addr = 0x1000 + m_io_addr;
+            system2200::unregisterKb(kb_addr, m_term_num);
+        }
     }
 
     m_init_tmr    = nullptr;
@@ -372,11 +419,18 @@ void
 Terminal::termToMxdCallback(int key)
 {
     m_tx_tmr = nullptr;
-    m_muxd->receiveKeystroke(m_term_num, key);
-
-    // poll for script input, but don't let it overrun the key buffer
-    if (m_kb_buff.size() < 5) {
-        m_script_active = system2200::pollScriptInput(m_io_addr+0x01, m_term_num);
+    
+    // if (m_muxd) {
+        // Original MUX mode - send to Wang 2200 CPU
+        m_muxd->receiveKeystroke(m_term_num, key);
+        
+        // poll for script input, but don't let it overrun the key buffer
+        if (m_kb_buff.size() < 5) {
+            m_script_active = system2200::pollScriptInput(m_io_addr+0x01, m_term_num);
+    //     }
+    // } else if (m_serialPort) {
+    //     // COM port mode - send to real Wang 2200 via serial port
+    //     m_serialPort->sendByte(static_cast<uint8>(key));
     }
 
     // see if any other chars are pending
@@ -1005,6 +1059,12 @@ Terminal::processCrtChar3(uint8 byte)
 
         case 0x03:      // clear screen
             clearScreen();
+// #ifdef STANDALONE_TERMINAL
+//             if (m_serialPort) {
+//                 // Clear console screen (Windows)
+//                 system("cls");
+//             }
+// #endif
             break;
 
         case 0x05:      // enable cursor
@@ -1021,6 +1081,12 @@ Terminal::processCrtChar3(uint8 byte)
 
         case 0x08:      // cursor left
             adjustCursorX(-1);
+// #ifdef STANDALONE_TERMINAL
+//             if (m_serialPort) {
+//                 printf("\b");
+//                 fflush(stdout);
+//             }
+// #endif
             break;
 
         case 0x09:      // horizontal tab
@@ -1029,6 +1095,12 @@ Terminal::processCrtChar3(uint8 byte)
 
         case 0x0A:      // linefeed (go down one column)
             adjustCursorY(+1);
+// #ifdef STANDALONE_TERMINAL
+//             if (m_serialPort) {
+//                 printf("\n");
+//                 fflush(stdout);
+//             }
+// #endif
             break;
 
         case 0x0C:      // reverse index (cursor up)
@@ -1037,6 +1109,12 @@ Terminal::processCrtChar3(uint8 byte)
 
         case 0x0D:      // go to first column
             setCursorX(0);
+// #ifdef STANDALONE_TERMINAL
+//             if (m_serialPort) {
+//                 printf("\r");
+//                 fflush(stdout);
+//             }
+// #endif
             break;
 
         default:        // just a character
